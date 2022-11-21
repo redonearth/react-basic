452e0a2c61e89f54639e9b047cefce91091484b3
Redonearth <wona23@gmail.com>
2022-11-22 01:54:10 +0900

    .gitignore 적용

---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <>
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+          alt="avatar"
+        />
+      </div>
+      <h1>James Hong</h1>
+      <div>프론트엔드 개발자</div>
+    </>
+  );
+}

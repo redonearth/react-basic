ec88aae1450e7cd6dd65673c17fb54be36ed7cde
Redonearth <wona23@gmail.com>
2022-11-24 19:30:45 +0900

    feat: useEffect 생애주기 로직 추가

---
+++ src/components/Products.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Products() {
+  const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
+
+  useEffect(() => {
+    fetch(`data/${checked ? 'sale_' : ''}products.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('🔥 뜨끈한 데이터를 네트워크에서 받아옴');
+        setProducts(data);
+      });
+    return () => {
+      console.log('🧹 깨끗하게 청소');
+    };
+  }, [checked]);
+
+  return (
+    <>
+      <input
+        id="checkbox"
+        type="checkbox"
+        value={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor="checkbox">🔥 세일 중인 제품만 보기</label>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}

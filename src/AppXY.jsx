1d06f5fb0cf09a0c3750dd2eae10b1c56904ac0f
Redonearth <wona23@gmail.com>
2022-11-26 08:33:25 +0900

    feat: 마우스 좌표 상태 로직 개선

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,21 +2,19 @@ import React, { useState } from 'react';
 import './styles/XY.css';
 
 export default function AppXY() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 200 });
 
   return (
     <div
       className="container"
       onPointerMove={(e) => {
         console.log(e.clientX, e.clientY);
-        setX(e.clientX);
-        setY(e.clientY);
+        setPosition((prev) => ({ ...prev, x: e.clientX }));
       }}
     >
       <div
         className="pointer"
-        style={{ transform: `translate(${x}px, ${y}px)` }}
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
       />
     </div>
   );

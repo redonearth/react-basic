13c681f2ac1947f0fdd13fa23d891b6cdebede1c
Redonearth <wona23@gmail.com>
2022-11-26 09:55:13 +0900

    feat: Reducer로 기존 로직 분리

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,34 +1,24 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+import personReducer from './reducer/person-reducer';
 
 export default function AppMentors() {
-  const [person, setPerson] = useState(initialPerson);
+  const [person, dispatch] = useReducer(personReducer, initialPerson);
+
   const handleUpdate = () => {
     const prev = prompt(`누구의 이름을 바꾸고 싶으세요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶으세요?`);
-    setPerson((person) => ({
-      ...person,
-      mentors: person.mentors.map((mentor) => {
-        if (mentor.name === prev) {
-          return { ...mentor, name: current };
-        }
-        return mentor;
-      }),
-    }));
+    dispatch({ type: 'updated', prev, current });
   };
+
   const handleAdd = () => {
     const name = prompt(`멘토의 이름은 무엇인가요?`);
     const title = prompt(`멘토의 타이틀은 무엇인가요?`);
-    setPerson((person) => ({
-      ...person,
-      mentors: [{ name, title }, ...person.mentors],
-    }));
+    dispatch({ type: 'added', name, title });
   };
+
   const handleDelete = () => {
     const name = prompt(`누구를 삭제하고 싶나요?`);
-    setPerson((person) => ({
-      ...person,
-      mentors: person.mentors.filter((mentor) => mentor.name !== name),
-    }));
+    dispatch({ type: 'deleted', name });
   };
 
   return (

11a66c687e49a683ff596e6ed96446e829d54ee2
Redonearth <wona23@gmail.com>
2022-11-30 08:14:27 +0900

    feat: PostCSS 적용 코드 구현

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppClass from './basic/AppClass';
+import App from './css/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppClass />
+    <App />
   </React.StrictMode>
 );
 

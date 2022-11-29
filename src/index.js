674601507b69aedbe61d17c9c986df5853e59251
Redonearth <wona23@gmail.com>
2022-11-30 08:06:00 +0900

    move: basic 프로젝트 이동

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppClass from './AppClass';
+import AppClass from './basic/AppClass';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
